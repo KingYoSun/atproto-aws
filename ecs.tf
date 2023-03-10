60a537f97c062c8a23c0eae2a366c7b9d4bca31f
KingYoSun <nocking257838@gmail.com>
2023-03-11 01:19:55 +0900

    fix ecs

--- ecs.tf
+++ ecs.tf
@@ -22,6 +22,10 @@ data "template_file" "atproto_pds_container_definitions" {
     image                 = "${var.atproto_pds_container_repo_url}:${var.atproto_pds_container_tag}",
     region                = var.region
     cloudwatch_group_name = aws_cloudwatch_log_group.atproto_pds.name
+    database_name         = data.aws_ssm_parameter.atproto_pds_database_name.arn
+    database_username     = data.aws_ssm_parameter.atproto_pds_database_username.arn
+    database_password     = data.aws_ssm_parameter.atproto_pds_database_password.arn
+    database_host         = aws_ssm_parameter.atproto_pds_database_url.arn
   }
 }
 
@@ -32,6 +36,8 @@ resource "aws_ecs_task_definition" "atproto_pds" {
   memory                   = 2048
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.atproto_pds_fargate-task-execution.arn
+  task_role_arn            = aws_iam_role.atproto_pds_fargate-task.arn
 
   depends_on = [
     aws_vpc_endpoint.atproto_pds_s3,
@@ -39,6 +45,65 @@ resource "aws_ecs_task_definition" "atproto_pds" {
     aws_vpc_endpoint.atproto_pds_secretmanager,
     aws_vpc_endpoint.atproto_pds_ssm,
   ]
+}
+
+resource "aws_ecs_service" "atproto_pds" {
+  name                               = "atproto_pds"
+  cluster                            = aws_ecs_cluster.atproto_pds.id
+  platform_version                   = "LATEST"
+  task_definition                    = aws_ecs_task_definition.atproto_pds.arn
+  desired_count                      = 1
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  propagate_tags                     = "SERVICE"
+  enable_execute_command             = true
+  launch_type                        = "FARGATE"
+  health_check_grace_period_seconds  = 60
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
+  network_configuration {
+    assign_public_ip = true
+    subnets          = [
+      aws_subnet.atproto_pds_public_a.id,
+    ]
+    security_groups = [
+      aws_security_group.atproto_pds_app.id,
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.atproto_pds.arn
+    container_name   = "atproto_pds"
+    container_port   = 80
+  }
+}
+
+resource "aws_lb_target_group" "atproto_pds" {
+  name                 = "atproto_pds"
+  vpc_id               = aws_vpc.atproto_pds.id
+  target_type          = "ip"
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 60
+  health_check { path = "/api/health_check" }
+}
+
+resource "aws_lb_listener_rule" "atproto_pds" {
+  listener_arn = aws_lb_listener.atproto_pds_https.arn
+  priority     = 2
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.atproto_pds.arn
+  }
+  condition {
+    host_header {
+      values = [var.host_domain]
+    }
+  }
 }
 
 resource "aws_iam_role" "atproto_pds_fargate-task" {
@@ -95,13 +160,19 @@ data "template_file" "atproto_pds_fargate-task-execution" {
   template = file("/policies/iam_role_policy/fargate-task-execution.json")
 
   vars = {
-    "ssm_arn"           = "value",
-    "secretmanager_arn" = "value",
-    "s3_arn"            = "value"
+    "ssm_arn"                   = "value",
+    "secretmanager_arn"         = "value",
+    "s3_arn"                    = "value",
+    "ssm_database_password_arn" = data.aws_ssm_parameter.atproto_pds_database_password.arn
   }
 }
 
 resource "aws_iam_role_policy_attachment" "atproto_pds_AmazonECSTaskExecutionRolePolicy" {
   role       = aws_iam_role.atproto_pds_fargate-task-execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "atproto_pds_AmazonSSMReadOnlyAccess" {
+  role       = aws_iam_role.atproto_pds_fargate-task-execution.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
--- rds.tf
+++ rds.tf
@@ -1,12 +1,12 @@
-data "aws_ssm_parameter" "database_name" {
+data "aws_ssm_parameter" "atproto_pds_database_name" {
   name = "${var.ssm_parameter_store_base}/database_name"
 }
 
-data "aws_ssm_parameter" "database_user" {
-  name = "${var.ssm_parameter_store_base}/database_user"
+data "aws_ssm_parameter" "atproto_pds_database_username" {
+  name = "${var.ssm_parameter_store_base}/database_username"
 }
 
-data "aws_ssm_parameter" "database_password" {
+data "aws_ssm_parameter" "atproto_pds_database_password" {
   name = "${var.ssm_parameter_store_base}/database_password"
 }
 
