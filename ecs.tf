d2cd042834392339de165097aad97e3427d3a804
KingYoSun <nocking257838@gmail.com>
2023-03-15 23:28:20 +0900

    change ecs to minimum server spec

--- ecs.tf
+++ ecs.tf
@@ -52,8 +52,8 @@ data "template_file" "atproto_pds_container_definitions" {
 resource "aws_ecs_task_definition" "atproto_pds" {
   family                   = "atproto_pds"
   container_definitions    = data.template_file.atproto_pds_container_definitions.rendered
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 256
+  memory                   = 512
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.atproto_pds_fargate-task-execution.arn
