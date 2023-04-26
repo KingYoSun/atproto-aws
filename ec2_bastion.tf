739a4b5526fb290cfdfb92860693095aba2a9be2
KingYoSun <nocking257838@gmail.com>
2023-04-26 23:34:07 +0900

    latest ami for bastion

--- ec2_bastion.tf
+++ ec2_bastion.tf
@@ -3,9 +3,13 @@ resource "aws_key_pair" "bastion" {
   public_key = var.bastion_pub_key
 }
 
+data aws_ssm_parameter amzn2_ami {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
 resource "aws_instance" "bastion" {
   # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  ami                         = "ami-e99f4896"
+  ami                         = data.aws_ssm_parameter.amzn2_ami.value
   instance_type               = "t2.micro"
   associate_public_ip_address = "true"
   key_name                    = aws_key_pair.bastion.key_name
